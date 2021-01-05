9ab6d06b53f90920ae6b952ce97996a4fdcbbc01
Chris Strybis <chris.strybis@gmail.com>
2021-01-05 18:34:11 +0200

    node shit

--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import Room
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
 from.serializers import RoomSerializer
 # Create your views here.
 
