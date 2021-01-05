9eaac1023f7137c59f74d665f3fca62f3a586005
Chris Strybis <chris.strybis@gmail.com>
2021-01-05 20:39:28 +0200

    pylint

--- api/views.py
+++ api/views.py
@@ -1,9 +1,10 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Room
-from django.shortcuts import render
 from rest_framework import generics, status
-from.serializers import RoomSerializer
+from django.shortcuts import render
+from .models import Room
+from .serializers import RoomSerializer
+
 # Create your views here.
 
 
