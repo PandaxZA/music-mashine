fda1054b661800b71315ff866fa6a44d498b1659
Chris Strybis <chris.strybis@gmail.com>
2021-01-10 20:59:28 +0200

    styling

--- frontend/src/components/MusicPlayer.js
+++ frontend/src/components/MusicPlayer.js
@@ -38,11 +38,11 @@ export default class MusicPlayer extends Component {
 
     return (
       <Card>
-        <Grid container alignItems="center">
-          <Grid item align="center" xs={4}>
+        <Grid container alignItems="center" justify="center">
+          <Grid item align="center" xs={12} sm={8}>
             <img src={this.props.image_url} height="100%" width="100%" />
           </Grid>
-          <Grid item align="center" xs={8}>
+          <Grid item align="center" xs={12} sm={4}>
             <Typography component="h5" variant="h5">
               {this.props.title}
             </Typography>
